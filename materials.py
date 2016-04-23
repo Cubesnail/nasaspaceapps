ff4f7a509d510a1fbd91e15c263766a6dff7ac65
mosiman <dillonchan97@gmail.com>
2016-04-22 23:06:36 -0400

    Added materials, people, plant class

---
+++ materials.py
@@ -0,0 +1,16 @@
+# MATERIALS
+
+
+class Material:
+	# __init__ : self, Str, Num, Num, Num
+
+	def __init__(self, name, mass, cost, density):
+		self.name = name
+		self.mass = mass
+		self.cost = cost
+		self.density = density
+	def get_volume(self.mass, self.density):
+		return self.mass / self.density
+
+
+

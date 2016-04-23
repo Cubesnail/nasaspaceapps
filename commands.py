7d22cdaf554815026b93233a3d066181f9b622ed
Cubesnail <cubesnail@gmail.com>
2016-04-23 01:38:03 -0400

    More commands update

--- commands.py
+++ commands.py
@@ -14,24 +14,45 @@ class Command:
 
 class Build(Command):
     #  TODO
-    pass
-
+    def __init__(self, building, location):
+        self.location = location
+    def do(self):
+        #  TODO
+        pass
 
 class Destroy(Command):
     #  TODO
-    pass
-
+    def __init__(self, location):
+        self.location = location
+    def do(self):
+        #  TODO
+        pass
 
 class Collect(Command):
     #  TODO
-    pass
-
+    def __init__(self,location):
+        self.location = location
+    def do(self):
+        #  TODO
+        pass
 
 class SendWorker(Command):
     #  TODO
-    pass
+    def __init__(self, worker, location):
+        self.location = location
+        self.worker = worker
+
+    def do(self):
+        #  TODO
+        pass
 
 class Research(Command):
     #  TODO
-    pass
+    def __init__(self, product, location):
+        self.location = location
+        self.product = product
+
+    def do(self):
+        #  TODO
+        pass
 

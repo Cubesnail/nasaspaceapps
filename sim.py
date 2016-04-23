f85889c050866c087d3d92b0d16f61fe52cc578e
mosiman <dillonchan97@gmail.com>
2016-04-23 01:39:07 -0400

    more work on sim.py, initializing graphics (map)

--- sim.py
+++ sim.py
@@ -1,6 +1,6 @@
 # SIMULATOR
 # main program
-
+from buildings import *
 import os # used for: clear, 
 
 class map:
@@ -22,30 +22,33 @@ class map:
 
         # create top border
         temp = []
-        for k in range(0, self.height+1):
+        for k in range(0, self.width+2):
             temp.append("-")
+        temp.append("\n")
         self.screen.append(temp)
 
         
-        for k in range(0, self.height+1):
+        for k in range(0, self.height):
             temp = ["|"] 
-            for x in range(0, self.width+1):
-                temp.append("O")
+            for x in range(0, self.width):
+                temp.append(None)
             temp.append("|")
+            #  temp.append('\n')
             self.screen.append(temp)
+
         # create bottom border
         temp = []
-        for k in range(0, self.height+1):
+        for k in range(0, self.width+2):
             temp.append("-")
         self.screen.append(temp)
 
     def display (self):
         # TODO
         
-        for k in range (0, self.height+1):
-            for x in range(0, self.width+1):
-                print(self.screen[k][x])
-
+        for k in range (0, self.height+2):
+            for x in range(0, self.width+2):
+                print(self.screen[k][x], end = '')
+            print()
     def update (self):
         # update
         pass
